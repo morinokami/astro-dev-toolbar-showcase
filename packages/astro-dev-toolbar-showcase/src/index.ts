64589bc0c4ba332c989d4125ecb68f59be0a7c13
Shinya Fujino <shf0811@gmail.com>
2025-04-23 00:13:15 +0900

    Add basic structure

--- packages/astro-dev-toolbar-showcase/src/index.ts
+++ packages/astro-dev-toolbar-showcase/src/index.ts
@@ -4,35 +4,16 @@ export default function myAstroIntegration(): AstroIntegration {
 	return {
 		name: "astro-dev-toolbar-showcase",
 		hooks: {
-			"astro:config:setup": ({ logger }) => {
-				logger.info("astro:config:setup");
-			},
-			"astro:config:done": ({ logger }) => {
-				logger.info("astro:config:done");
-			},
-			"astro:server:setup": ({ logger }) => {
-				logger.info("astro:server:setup");
-			},
-			"astro:server:start": ({ logger }) => {
-				logger.info("astro:server:start");
-			},
-			"astro:server:done": ({ logger }) => {
-				logger.info("astro:server:done");
-			},
-			"astro:build:start": ({ logger }) => {
-				logger.info("astro:build:start");
-			},
-			"astro:build:setup": ({ logger }) => {
-				logger.info("astro:build:setup");
-			},
-			"astro:build:generated": ({ logger }) => {
-				logger.info("astro:build:generated");
-			},
-			"astro:build:ssr": ({ logger }) => {
-				logger.info("astro:build:ssr");
-			},
-			"astro:build:done": ({ logger }) => {
-				logger.info("astro:build:done");
+			"astro:config:setup": ({ addDevToolbarApp }) => {
+				addDevToolbarApp({
+					id: "astro-dev-toolbar-showcase",
+					name: "Astro Dev Toolbar Showcase",
+					icon: "astro:logo",
+					entrypoint: new URL(
+						"./astro-dev-toolbar-showcase.mjs",
+						import.meta.url,
+					),
+				});
 			},
 		},
 	};
